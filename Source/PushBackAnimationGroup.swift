a7f2829820b8b3cf2b9ececdcdd6c010f3c041c1
denism <demaltsev@gmail.com>
2020-03-04 12:32:44 +0300

    Removed 3D rotation animation

--- Source/PushBackAnimationGroup.swift
+++ Source/PushBackAnimationGroup.swift
@@ -9,18 +9,6 @@ public class PushBackAnimationGroup: CAAnimationGroup {
         id1.m34 = 1.0 / -900
         id1 = CATransform3DScale(id1, 0.95, 0.95, 1)
         
-        let angleFactor: CGFloat = UIDevice.isPad() ? 7.5 : 15.0
-        id1 = CATransform3DRotate(id1, angleFactor * CGFloat(Double.pi) / 180.0, 1, 0, 0)
-        
-        var id2 = CATransform3DIdentity
-        id2.m34 = id1.m34
-        
-        let scale = CGFloat(options[.parentScale] as! Double)
-        let tzFactor: CGFloat = UIDevice.isPad() ? -0.04 : -0.08
-        
-        id2 = CATransform3DTranslate(id2, 0, viewHeight * tzFactor, 0)
-        id2 = CATransform3DScale(id2, scale, scale, 1)
-        
         let animation = CABasicAnimation(keyPath: "transform")
         animation.toValue = NSValue(caTransform3D: id1)
         
@@ -39,7 +27,7 @@ public class PushBackAnimationGroup: CAAnimationGroup {
         
         fillMode = CAMediaTimingFillMode.forwards
         isRemovedOnCompletion = false
-        duration = animation.duration * 2
-        animations = [animation, animation2]
+        duration = animation.duration
+        animations = [animation]
     }
 }
